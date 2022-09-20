0d8b73d2810d6f3422b22f30c40c075db9b2eb11
jf vo <jef.anhduc@gmail.com>
2022-09-20 11:12:42 -0400

    Part 2.2

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ from functions import count_labels, print_graph, get_static_file
 import json
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 
-
+# Part 1
 file = open(get_static_file('goemotions.json'), "r")
 comments = np.array(json.load(file))
 file.close()
@@ -14,6 +15,7 @@ sentiments = count_labels(2, comments)
 # print_graph(emotions, 'emotions.pdf')
 # print_graph(sentiments, 'sentiments.pdf')
 
+# Part 2.1
 #TODO: fix memory issue
 text_comments = [comment[0] for comment in comments]
 vectorizer = CountVectorizer()
@@ -22,4 +24,8 @@ list_features = vectorizer.get_feature_names_out()
 list_count = cv_fit.toarray().sum(axis=0)
 features_count = dict(zip(list_features, list_count))
 
-print(features_count)
+#Part 2.2
+train_batch, test_batch = np.array(train_test_split(comments, train_size=0.8, test_size=0.2))
+# print("full batch size: ", comments.size)
+# print("train batch size: ", train_batch.size)
+# print("test batch size:" , test_batch.size)
